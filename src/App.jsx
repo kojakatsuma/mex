4e71bf345a2bf4cff4fd405c7007bb2e2422f938
kojakatsuma <katsuma.koja@ubiregi.com>
2020-01-29 16:34:57 +0900

    update

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,14 @@
 import React from 'react';
-import { Container, Typography, Paper } from '@material-ui/core';
+import { Container, Typography } from '@material-ui/core';
+import { Post } from './Post';
 
 export default () => {
-  document.title = 'mex'
   return (
     <Container maxWidth="lg">
       <Typography variant="h1" >mex</Typography>
       <Typography variant="h2" >here is mexico.</Typography>
       <Container maxWidth="md">
-        {[...Array(60).keys()].map(i => <Paper square elevation={0} key={i}>
-          <Typography variant="h3" >Caption{i}</Typography>
-          <Typography variant="body2">{"2020.01.29"}</Typography>
-          <Typography variant="body1" noWrap>HHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHH</Typography>
-        </Paper>)}
+        {[...Array(60).keys()].map(i => <Post key={i} caption={`CAPTION No.${i}`} date={"2019.12.09"} summary={'HHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHH'} />)}
       </Container>
     </Container>
   )
