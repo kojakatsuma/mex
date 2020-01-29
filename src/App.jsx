aba446620aa961bcc8b9633ce163c9cf1e13d096
kojakatsuma <katsuma.koja@ubiregi.com>
2020-01-29 16:17:00 +0900

    update

--- src/App.jsx
+++ src/App.jsx
@@ -5,12 +5,13 @@ export default () => {
   document.title = 'mex'
   return (
     <Container maxWidth="lg">
-      <Typography component={'div'} variant="h1" >mex</Typography>
+      <Typography variant="h1" >mex</Typography>
+      <Typography variant="h2" >here is mexico.</Typography>
       <Container maxWidth="md">
-        {[...Array(60).keys()].map(i => <Paper square  elevation={0} key={i}>
+        {[...Array(60).keys()].map(i => <Paper square elevation={0} key={i}>
           <Typography variant="h3" >Caption{i}</Typography>
           <Typography variant="body2">{"2020.01.29"}</Typography>
-          <Typography variant="body1">H</Typography>
+          <Typography variant="body1" noWrap>HHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHH</Typography>
         </Paper>)}
       </Container>
     </Container>
