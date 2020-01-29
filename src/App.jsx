a7a1fddbef09066694fd7e6fe9d73fd97dca9ddc
kojakatsuma <katsuma.koja@ubiregi.com>
2020-01-29 15:09:26 +0900

    update

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,18 @@
 import React from 'react';
-import { Container } from '@material-ui/core';
+import { Container, Typography, Paper } from '@material-ui/core';
 
 export default () => {
+  document.title = 'mex'
   return (
-    <Container maxWidth="sm">
-      <h1>Hello World !!!</h1>
+    <Container maxWidth="lg">
+      <Typography component={'div'} variant="h1" >mex</Typography>
+      <Container maxWidth="md">
+        {[...Array(60).keys()].map(i => <Paper square  elevation={0} key={i}>
+          <Typography variant="h3" >Caption{i}</Typography>
+          <Typography variant="body2">{"2020.01.29"}</Typography>
+          <Typography variant="body1">H</Typography>
+        </Paper>)}
+      </Container>
     </Container>
   )
 }
