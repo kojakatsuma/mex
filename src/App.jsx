713a2ecc2d1d0cdba01a0cb5d6e581e1da213258
kojakatsuma <katsuma.koja@ubiregi.com>
2020-02-01 22:17:02 +0900

    update

--- src/App.jsx
+++ src/App.jsx
@@ -1,29 +1,38 @@
 import React from 'react';
-import { Container, Typography, Paper, Divider } from '@material-ui/core';
+import { Container, Typography, Paper, Divider, createMuiTheme, ThemeProvider } from '@material-ui/core';
 import { Posts } from './mdx';
-import 'github-markdown-css';
-import './index.css';
+import 'github-markdown-css'
+import './index.css'
+
+
+const theme = createMuiTheme({
+  typography: {
+    fontFamily: "'M PLUS Rounded 1c'",
+  },
+})
 
 export default () => {
   return (
-    <Container maxWidth="md">
-      <Typography variant="h1" >mex</Typography>
-      <Typography variant="subtitle2" paragraph>here is mexico.</Typography>
-      {
-        Posts.map((Post, i) => {
-          return (
-            <React.Fragment key={i}>
-              <Divider />
-              <Paper style={{ marginBottom: '2em' }} square elevation={0} className="markdown-body">
-                <Typography component={'span'} >
-                <Post />
-                </Typography>
-              </Paper>
-            </React.Fragment>
-          )
-        })
-      }
+    <ThemeProvider theme={theme}>
+      <Container maxWidth="md">
+        <Typography variant="h2" >mex</Typography>
+        <Typography variant="subtitle2" paragraph>here is mexico.</Typography>
+        {
+          Posts.map((Post, i) => {
+            return (
+              <React.Fragment key={i}>
+                <Divider />
+                <Paper style={{ marginBottom: '2em' }} square elevation={0} className='markdown-body' >
+                  <Typography>
+                    <Post />
+                  </Typography>
+                </Paper>
+              </React.Fragment>
+            )
+          })
+        }
 
-    </Container>
+      </Container>
+    </ThemeProvider>
   )
 }
