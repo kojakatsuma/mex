364dd79bde51aabc239c31e6f73e376c63bdb80a
kojakatsuma <katsuma.koja@ubiregi.com>
2020-02-02 01:23:01 +0900

    update

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
-import React from 'react';
-import { Container, Typography, Paper, Divider, createMuiTheme, ThemeProvider, CssBaseline } from '@material-ui/core';
+import React, { useEffect, useState } from 'react';
+import { Container, Typography, Paper, Divider, createMuiTheme, ThemeProvider, CssBaseline, Fade } from '@material-ui/core';
 import { Posts } from './mdx';
+import FontFaceObserver from 'fontfaceobserver';
 import 'github-markdown-css'
 import './index.css'
 
@@ -38,27 +39,38 @@ const theme = createMuiTheme({
 })
 
 export default () => {
+  const [loaded, setLoaded] = useState(false)
+  useEffect(() => {
+    const font = new FontFaceObserver('MPLUSRounded1c')
+    font.load().then(() => {
+      setLoaded(true)
+    })
+  })
   return (
     <ThemeProvider theme={theme}>
       <Container maxWidth="md">
-      <CssBaseline />
-        <Typography variant="h2" >mex</Typography>
-        <Typography variant="subtitle2" paragraph>here is mexico.</Typography>
-        {
-          Posts.map((Post, i) => {
-            return (
-              <React.Fragment key={i}>
-                <Divider />
-                <Paper style={{ marginBottom: '2em' }} square elevation={0} className='markdown-body' >
-                  <Typography component='span'>
-                    <Post />
-                  </Typography>
-                </Paper>
-              </React.Fragment>
-            )
-          })
+        <CssBaseline />
+        {loaded ? <Fade in={loaded} timeout={{ enter: 800 }}>
+          <Paper elevation={0}>
+            <Typography variant="h2" >mex</Typography>
+            <Typography variant="subtitle2" paragraph>here is mexico.</Typography>
+            {
+              Posts.map((Post, i) => {
+                return (
+                  <React.Fragment key={i}>
+                    <Divider />
+                    <Paper style={{ marginBottom: '2em' }} square elevation={0} className='markdown-body' >
+                      <Typography component='span' >
+                        <Post />
+                      </Typography>
+                    </Paper>
+                  </React.Fragment>
+                )
+              })
+            }
+          </Paper>
+        </Fade> : <div>loading</div>
         }
-
       </Container>
     </ThemeProvider>
   )
