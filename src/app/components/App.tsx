6d7fc6fee589a1666498a8e1a041513eec7f71f9
kojakatsuma <katsuma.koja@ubiregi.com>
2020-03-02 22:59:30 +0900

    update

--- src/app/components/App.tsx
+++ src/app/components/App.tsx
@@ -1,17 +1,17 @@
 import Header from './Header';
-import {Posts} from './mdx';
+import { Posts } from './mdx';
 import { Typography, Divider } from '@material-ui/core';
 
 const App = () => (
   <main>
     <Header />
     {
-      Posts.map((Post,i)=>{
+      Posts.map((Post, i) => {
         return (
-        <Typography component='article' key={i}>
-           <Post/>
-          <Divider />
-        </Typography>
+          <Typography component='article' key={i}>
+            <Post />
+            <Divider />
+          </Typography>
         )
       })
     }
