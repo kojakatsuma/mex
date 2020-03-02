7fd3a80a20d8f269aa69598ed8f643cc08d02cd7
kojakatsuma <katsuma.koja@ubiregi.com>
2020-03-03 00:27:46 +0900

    update

--- src/app/pages/_app.tsx
+++ src/app/pages/_app.tsx
@@ -3,12 +3,23 @@ import { createMuiTheme, ThemeProvider } from '@material-ui/core/styles';
 import { AppProps } from 'next/app';
 import 'prism-themes/themes/prism-vs.css';
 
+const colorCode = '#ffffff'
+
+const theme = createMuiTheme({
+  palette: {
+    background: {
+      paper: colorCode,
+      default: colorCode,
+    },
+  },
+})
+
 function MyApp({ Component, pageProps }: AppProps) {
-  return(
-  <ThemeProvider theme={createMuiTheme({})}>
-  <CssBaseline />
-  <Component {...pageProps} />
-</ThemeProvider>
+  return (
+    <ThemeProvider theme={theme}>
+      <CssBaseline />
+      <Component {...pageProps} />
+    </ThemeProvider>
   )
 }
 
