7860be1b911880a7fafa98ffeb3d9d4d4b89d0bc
kojakatsuma <katsuma.koja@ubiregi.com>
2020-03-27 08:52:21 +0900

    update

--- src/app/components/App.tsx
+++ src/app/components/App.tsx
@@ -2,7 +2,7 @@ import { Posts } from './mdx';
 import { Typography, Divider, Container, Paper } from '@material-ui/core';
 
 const App = () => (
-  <Container maxWidth={false} style={{ maxWidth: 680 }}>
+  <Container maxWidth={false} style={{ maxWidth: 900 }}>
     <Paper elevation={0}>
       {
         Posts.map((Post, i) => {
