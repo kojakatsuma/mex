d6e0507181510396b1f5b0d33f3c6f7cc47a5433
kojakatsuma <katsuma.koja@ubiregi.com>
2020-05-13 01:46:45 +0900

    update

--- pages/text/index.tsx
+++ pages/text/index.tsx
@@ -9,6 +9,9 @@ export default () => {
       <Link href='text/2'>
         <h2 className='menu'>オンラインセッションを録音する</h2>
       </Link>
+      <Link href='text/3'>
+        <h2 className='menu'>「可傷的な歴史(ロードムービー)」を観た</h2>
+      </Link>
       <a className='menu' href='https://qiita.com/koja1234'>
         <h2>Qiitaの投稿記事</h2>
       </a>
