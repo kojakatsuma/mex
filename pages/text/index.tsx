644f42cd6df95690e8bd1d17763e89b354934d09
kojakatsuma <katsuma.koja@ubiregi.com>
2020-05-13 02:01:14 +0900

    update

--- pages/text/index.tsx
+++ pages/text/index.tsx
@@ -4,16 +4,16 @@ export default () => {
   return (
     <>
       <Link href='text/1'>
-        <h2 className='menu'>生活を改善する3月</h2>
+        <h3 className='menu'>生活を改善する3月</h3>
       </Link>
       <Link href='text/2'>
-        <h2 className='menu'>オンラインセッションを録音する</h2>
+        <h3 className='menu'>オンラインセッションを録音する</h3>
       </Link>
       <Link href='text/3'>
-        <h2 className='menu'>「可傷的な歴史(ロードムービー)」を観た</h2>
+        <h3 className='menu'>「可傷的な歴史(ロードムービー)」を観た</h3>
       </Link>
       <a className='menu' href='https://qiita.com/koja1234'>
-        <h2>Qiitaの投稿記事</h2>
+        <h3>Qiitaの投稿記事</h3>
       </a>
     </>
   );
