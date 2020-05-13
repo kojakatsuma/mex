66b711cdaf114424a552d0feb9c8bda99ea6fe90
kojakatsuma <katsuma.koja@ubiregi.com>
2020-05-14 02:05:58 +0900

    remove ogp parser

--- next.config.js
+++ next.config.js
@@ -2,13 +2,11 @@
 const rehypePrism = require('@mapbox/rehype-prism')
 const heading = require('remark-autolink-headings')
 const slug = require('remark-slug')
-const ogp = require('remark-ogp')
 
 
 const withMDX = require('@next/mdx')({
   options: {
     remarkPlugins: [
-      ogp,
       slug,
       [
         heading, {
