07e4a34595d453ba8d3524e6f511bd0ec965cf20
kojakatsuma <katsuma.koja@ubiregi.com>
2020-05-14 00:56:55 +0900

    parse ogp header

--- next.config.js
+++ next.config.js
@@ -2,18 +2,20 @@
 const rehypePrism = require('@mapbox/rehype-prism')
 const heading = require('remark-autolink-headings')
 const slug = require('remark-slug')
+const ogp = require('remark-ogp')
 
 
 const withMDX = require('@next/mdx')({
   options: {
     remarkPlugins: [
+      ogp,
       slug,
       [
         heading, {
           behavior: 'wrap',
           linkProperties: { class: 'inactive-link' }
         }
-      ]
+      ],
     ],
     rehypePlugins: [rehypePrism],
   },
