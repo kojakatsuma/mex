a5e893c26b04fa7776f03e2c29c195a86eb4be87
kojakatsuma <katsuma.koja@ubiregi.com>
2020-05-14 11:55:50 +0900

    update

--- components/mdx/index.jsx
+++ components/mdx/index.jsx
@@ -18,9 +18,11 @@ import a20200403 from './20200403.mdx';
 import a20200409 from './20200409.mdx';
 import a20200412 from './20200412.mdx';
 import a20200425 from './20200425.mdx';
+import a20200514 from './20200514.mdx';
 
 
 export const Posts = [
+  a20200514,
   a20200425,
   a20200412,
   a20200409,
