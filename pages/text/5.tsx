9dd7b16cf98f00ea7bb8f321ee44f7f9b2900ab1
kojakatsuma <katsuma.koja@ubiregi.com>
2020-05-23 00:12:59 +0900

    update

---
+++ pages/text/5.tsx
@@ -0,0 +1,17 @@
+
+import { Text, Props } from '../../components/Text';
+
+export async function getStaticProps() {
+  const data = await fetch('https://notion-api.splitbee.io/v1/page/65e0edf385b544aba98f89a709fd2197').then((res) =>
+    res.json(),
+  );
+  return {
+    props: {
+      url: '/text/5',
+      blockMap: data,
+    },
+  };
+}
+
+export default (props: Props) => <Text {...props} />;
+
--- pages/text/index.tsx
+++ pages/text/index.tsx
@@ -15,6 +15,9 @@ export default () => {
       <Link href='text/4'>
         <h3 className='menu'>react-router-domのSwitchは必要？</h3>
       </Link>
+      <Link href='text/5'>
+        <h3 className='menu'>react-routerとreact-router-domの違い</h3>
+      </Link>
       <a className='menu' href='https://qiita.com/koja1234'>
         <h3>Qiitaの投稿記事</h3>
       </a>
