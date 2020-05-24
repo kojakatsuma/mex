072be66cb25249c68cf98d0eb484e8ce99e8220c
kojakatsuma <katsuma.koja@ubiregi.com>
2020-05-24 23:36:44 +0900

    update

--- pages/text/index.tsx
+++ pages/text/index.tsx
@@ -3,23 +3,23 @@ import Link from 'next/link';
 export default () => {
   return (
     <>
-      <Link href='text/1'>
-        <h3 className='menu'>生活を改善する3月</h3>
-      </Link>
-      <Link href='text/2'>
-        <h3 className='menu'>オンラインセッションを録音する</h3>
-      </Link>
-      <Link href='text/3'>
-        <h3 className='menu'>「可傷的な歴史(ロードムービー)」を観た</h3>
-      </Link>
-      <Link href='text/4'>
-        <h3 className='menu'>react-router-domのSwitchは必要？</h3>
+      <Link href='text/6'>
+        <h3 className='menu'>オンラインセッションを録音する その2</h3>
       </Link>
       <Link href='text/5'>
         <h3 className='menu'>react-routerとreact-router-domの違い</h3>
       </Link>
-      <Link href='text/6'>
-        <h3 className='menu'>オンラインセッションを録音する その2</h3>
+      <Link href='text/4'>
+        <h3 className='menu'>react-router-domのSwitchは必要？</h3>
+      </Link>
+      <Link href='text/3'>
+        <h3 className='menu'>「可傷的な歴史(ロードムービー)」を観た</h3>
+      </Link>
+      <Link href='text/2'>
+        <h3 className='menu'>オンラインセッションを録音する</h3>
+      </Link>
+      <Link href='text/1'>
+        <h3 className='menu'>生活を改善する3月</h3>
       </Link>
       <a className='menu' href='https://qiita.com/koja1234'>
         <h3>Qiitaの投稿記事</h3>
