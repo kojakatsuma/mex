dccf086a5ed4f98fbf5e632f833a44995b063e9d
kojakatsuma <katsuma.koja@ubiregi.com>
2020-06-30 21:46:15 +0900

    update

--- scripts/makelog.js
+++ scripts/makelog.js
@@ -1,7 +1,14 @@
-const fs = require('fs')
-const {DateTime} = require("luxon")
+const fs = require('fs');
+const { DateTime } = require('luxon');
 
-const fileName = `components/mdx/${DateTime.local().toFormat('yyyyMMdd')}.mdx`
-const date = `## ${DateTime.local().toFormat('yyyy.MM.dd')}`
+let fileName = `components/mdx/${DateTime.local().toFormat('yyyyMMdd')}.mdx`;
+let count = 1;
 
-fs.appendFileSync(fileName,date,'utf8')
+while (fs.existsSync(fileName)) {
+  fileName = `components/mdx/${DateTime.local().toFormat('yyyyMMdd')}-${count}.mdx`;
+  count++;
+}
+
+const date = `## ${DateTime.local().toFormat('yyyy.MM.dd')}`;
+
+fs.appendFileSync(fileName, date, 'utf8');
