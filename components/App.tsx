bdc506f2a1919d97d34e797421d0476de8eb499f
kojakatsuma <katsuma.koja@ubiregi.com>
2020-08-02 19:23:27 +0900

    update

--- components/App.tsx
+++ components/App.tsx
@@ -1,6 +1,7 @@
 import { Posts } from './mdx';
 import { OGPHeader } from './OGPHeader';
 import Link from 'next/link';
+import { Fragment } from 'react';
 
 const App = () => {
   return (
@@ -8,13 +9,13 @@ const App = () => {
       <OGPHeader url={'/log'} title='log' metaDescription={'日記です'} />
       {Posts.map((Post, i) => {
         return (
-          <div key={i}>
+          <Fragment key={i}>
             <Post />
             <Link href='/'>
               <h3 className='menu'>{'back to top'}</h3>
             </Link>
             <hr />
-          </div>
+          </Fragment>
         );
       })}
     </>
