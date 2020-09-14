0679ebc43913e54da510ed0bb8a7ad9feb7c22e8
kojakatsuma <katsuma.koja@ubiregi.com>
2020-09-14 17:01:08 +0900

    debug log

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,6 +1,7 @@
 import Document, { Html, Head, Main, NextScript } from 'next/document';
 
 import { GA_ID } from '../libs/gtag';
+console.log(GA_ID)
 export default class MyDocument extends Document {
   render() {
     return (
