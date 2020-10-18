3be9a89d4abe0c095220eddbcbfc7c70405dc6f7
kojakatsuma <katsuma.koja@ubiregi.com>
2020-10-18 13:26:39 +0900

    不要な変数を削除

--- pages/text/index.tsx
+++ pages/text/index.tsx
@@ -3,16 +3,16 @@ import Link from 'next/link';
 import { BlockMapType } from 'react-notion';
 import { OGPHeader } from '../../components/OGPHeader';
 
-export const getStaticProps: GetStaticProps<{ links: { url: string; title: string; id: string }[] }> = async () => {
+export const getStaticProps: GetStaticProps<{ links: { url: string; title: string; }[] }> = async () => {
   const data: BlockMapType = await fetch(
     'https://notion-api.splitbee.io/v1/page/3495da64c3fa419a83d0f53cd8a671df',
   ).then((res) => res.json());
-  const links: { url: string; title: string; id: string }[] = await Promise.all(
+  const links: { url: string; title: string; }[] = await Promise.all(
     Object.entries(data)
       .filter(([_key, { value }], i) => i !== 0 && value.type === 'page')
-      .map(([key, { value }], index, pages) => {
+      .map(([_key, { value }], index, pages) => {
         const title = value.properties.title[0][0];
-        return { url: `text/${pages.length - index}`, title, id: `text/${key}` };
+        return { url: `text/${pages.length - index}`, title };
       }),
   );
   return {
@@ -23,10 +23,10 @@ export const getStaticProps: GetStaticProps<{ links: { url: string; title: strin
   };
 };
 
-const Text: React.FC<{ links: { url: string; title: string; id: string }[] }> = ({ links }) => (
+const Text: React.FC<{ links: { url: string; title: string }[] }> = ({ links }) => (
   <>
     <OGPHeader url={'/log'} title='log' metaDescription={'日記です'} />
-    {links.map(({ url: href, title, id }, i) => (
+    {links.map(({ url: href, title }, i) => (
       <Link key={i} href='/text/[id]' as={href}>
         <h3 className='menu'>{title}</h3>
       </Link>
