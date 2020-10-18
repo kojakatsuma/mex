58337088995e3b21e03c2a5a8c53a245abd0e7f1
kojakatsuma <katsuma.koja@ubiregi.com>
2020-10-18 22:46:21 +0900

    とりあえずこれ

--- components/Animation.tsx
+++ components/Animation.tsx
@@ -4,36 +4,57 @@ import React, { useRef, useEffect } from 'react';
 const sketch = (p: p5) => {
   p.setup = () => {
     p.createCanvas(p.windowWidth, p.windowHeight);
+    p.frameRate(10);
   };
-  let theta = 0;
+
   p.draw = () => {
+    let theta = 0;
     p.background(255);
-    p.stroke(200);
-    p.stroke(200);
     const a = (p.frameCount / p.width) * 180;
     if (a > 40) {
       p.noLoop();
     }
     theta = p.radians(a);
     p.translate(p.width / 2, p.height);
-    p.line(0, 0, 0, -100);
     p.translate(0, -100);
-    branch(40);
+    branch(10, theta);
+    branch2(10, -theta);
   };
-  const branch = (h = 400) => {
+  const branch = (h = 400, theta) => {
     h *= 0.66;
     if (h > 1) {
       p.push();
+      p.stroke(p.random(100), p.random(100), p.random(255), 100);
       p.rotate(theta);
-      p.line(0, 0, 0, -h);
-      p.translate(0, -h);
-      branch(h);
+      p.line(0, 0, 0, -h - p.random(20));
+      p.translate(0, -h - p.random(20));
+      branch(h, theta);
       p.pop();
       p.push();
+      p.stroke(p.random(100), p.random(100), p.random(200), 100);
       p.rotate(-theta);
-      p.line(0, 0, 0, -h);
-      p.translate(0, -h);
-      branch(h);
+      p.line(0, 0, 0, -h - p.random(20));
+      p.translate(0, -h - p.random(20));
+      branch(h, theta);
+      p.pop();
+    }
+  };
+  const branch2 = (h = 400, theta) => {
+    h *= 0.66;
+    if (h > 1) {
+      p.push();
+      p.stroke(p.random(200), p.random(100), p.random(100), 100);
+      p.rotate(-theta);
+      p.line(0, 0, 0, h + p.random(20));
+      p.translate(0, h + p.random(20));
+      branch2(h, -theta);
+      p.pop();
+      p.push();
+      p.stroke(p.random(100), p.random(200), p.random(100), 100);
+      p.rotate(theta);
+      p.line(0, 0, 0, h + p.random(20));
+      p.translate(0, h + p.random(20));
+      branch2(h, -theta);
       p.pop();
     }
   };
