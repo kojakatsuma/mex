d2d5ce54b76c1696e346065348ea83b1aa1a054f
kojakatsuma <katsuma.koja@ubiregi.com>
2021-03-09 01:08:12 +0900

    lazy load alt

--- components/Log.tsx
+++ components/Log.tsx
@@ -1,4 +1,5 @@
-import { NotionRenderer, BlockMapType, BaseTextValueType, TweetType } from 'react-notion';
+import { NotionRenderer, BlockMapType, BaseTextValueType, TweetType, classNames, getTextContent } from 'react-notion';
+import { Fragment } from 'react';
 import Link from 'next/link';
 import { TweetEmbed } from './TweetEmbed';
 import { Lazy } from './Lazy';
@@ -16,7 +17,7 @@ export const Log: React.FC<{ blockMaps: BlockMapType[] }> = ({ blockMaps }) => {
           const { properties: titleprops } = Object.values(blockMap)[0].value as BaseTextValueType;
           const title = titleprops?.title[0][0] || '';
           return (
-            <Lazy key={i}>
+            <Fragment key={i}>
               <div className='log-text'>
                 <h2 id={title}>
                   <a href={`#${title}`} className='inactive-link'>
@@ -30,13 +31,49 @@ export const Log: React.FC<{ blockMaps: BlockMapType[] }> = ({ blockMaps }) => {
                       const tweetValue = blockValue as TweetType;
                       return <TweetEmbed embed={tweetValue.embed} />;
                     },
+                    bookmark: ({ blockValue }: { blockValue: any }) => {
+                      const link = blockValue.properties.link;
+                      const title = blockValue.properties.title ?? link;
+                      const description = blockValue.properties.description;
+
+                      const block_color = blockValue.format?.block_color;
+                      const bookmark_icon = blockValue.format?.bookmark_icon;
+                      const bookmark_cover = blockValue.format?.bookmark_cover;
+                      return (
+                        <Lazy>
+                          <div className='notion-row'>
+                            <a
+                              target='_blank'
+                              rel='noopener noreferrer'
+                              className={classNames('notion-bookmark', block_color && `notion-${block_color}`)}
+                              href={link[0][0]}
+                            >
+                              <div>
+                                <div className='notion-bookmark-title'>{title}</div>
+                                {description && <div className='notion-bookmark-description'>{description}</div>}
+
+                                <div className='notion-bookmark-link'>
+                                  {bookmark_icon && <img src={bookmark_icon} alt={getTextContent(title)} />}
+                                  <div>{link}</div>
+                                </div>
+                              </div>
+                              {bookmark_cover && (
+                                <div className='notion-bookmark-image'>
+                                  <img src={bookmark_cover} alt={getTextContent(title)} />
+                                </div>
+                              )}
+                            </a>
+                          </div>
+                        </Lazy>
+                      );
+                    },
                   }}
                 />
                 <Link href='/'>
                   <h3 className='menu'>{'back to top'}</h3>
                 </Link>
               </div>
-            </Lazy>
+            </Fragment>
           );
         })}
     </>
